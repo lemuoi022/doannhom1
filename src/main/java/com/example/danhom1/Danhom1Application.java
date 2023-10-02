831c2f874f0ee730fdf51d5cbfe05c25670d8ba5
Muoi Le <muoi0886@gmail.com>
2023-10-03 01:08:05 +0700

    new

---
+++ src/main/java/com/example/danhom1/Danhom1Application.java
@@ -0,0 +1,13 @@
+package com.example.danhom1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Danhom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Danhom1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/danhom1/Danhom1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danhom1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Danhom1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
