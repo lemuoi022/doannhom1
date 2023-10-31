2604a1f875babace8cbd2aeebc11e28949e25eab
DuckTheMug <duc.12.2002@gmail.com>
2023-10-31 16:30:42 +0700

    revert

--- src/test/java/com/example/danhom1/Danhom1ApplicationTests.java
+++ src/test/java/com/example/danhom1/Danhom1ApplicationTests.java
@@ -1,10 +1,9 @@
 package com.example.danhom1;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@SpringBootTest
 class Danhom1ApplicationTests {
 
 	@Test
