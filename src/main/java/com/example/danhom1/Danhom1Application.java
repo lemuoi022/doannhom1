24b73184fd813969220a1efbe58b6dad1b583ceb
DuckTheMug <duc.12.2002@gmail.com>
2023-10-31 16:33:13 +0700

    disable Spring Security auto setup

--- src/main/java/com/example/danhom1/Danhom1Application.java
+++ src/main/java/com/example/danhom1/Danhom1Application.java
@@ -3,7 +3,7 @@ package com.example.danhom1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class Danhom1Application {
 
 	public static void main(String[] args) {
