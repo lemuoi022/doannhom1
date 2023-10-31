027772849dffeedbec64ad8d1424bc35fc0f557c
DuckTheMug <duc.12.2002@gmail.com>
2023-10-31 16:28:31 +0700

    disable spring security authorization auto setup

--- src/test/java/com/example/danhom1/Danhom1ApplicationTests.java
+++ src/test/java/com/example/danhom1/Danhom1ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.danhom1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 class Danhom1ApplicationTests {
