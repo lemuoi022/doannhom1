f2f625f70315913726f3d5857635f21bc0cd46ef
DuckTheMug <duc.12.2002@gmail.com>
2023-11-01 23:39:07 +0700

    initial UserStorageRepo

---
+++ src/main/java/com/example/danhom1/Storage/UserStorageRepo.java
@@ -0,0 +1,5 @@
+package com.example.danhom1.Storage;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserStorageRepo extends CrudRepository<UserStorage, Integer> { }
