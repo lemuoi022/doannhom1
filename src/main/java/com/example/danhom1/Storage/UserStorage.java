d898c7d4daedadb918df3204c30cc4b08c0834c9
DuckTheMug <duc.12.2002@gmail.com>
2023-11-01 23:38:47 +0700

    changes and first attempt to link UserStorage.java

--- src/main/java/com/example/danhom1/Storage/UserStorage.java
+++ src/main/java/com/example/danhom1/Storage/UserStorage.java
@@ -2,22 +2,30 @@ package com.example.danhom1.Storage;
 
 import com.example.danhom1.User.User;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 @Getter
+@Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_storage")
 public class UserStorage extends Storage {
 
     @Id
     @NonNull
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Setter
     @NonNull
-    @OneToOne
-    @JoinColumn(name = "userID")
+    @NotEmpty
+    @Column(name = "userLimit", nullable = false)
+    private Float userLimit = super.getDefaultLimit();
+
+    @NonNull
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userID",referencedColumnName = "id")
     private User user = new User();
 }
--- src/main/java/com/example/danhom1/User/User.java
+++ src/main/java/com/example/danhom1/User/User.java
@@ -1,34 +1,41 @@
 package com.example.danhom1.User;
 
+import com.example.danhom1.Storage.UserStorage;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
+@Getter
 @Data
 @Setter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter
     @NonNull
+    @Column(name = "id", nullable = false)
     private Integer id;
 
-    @Getter
     @NonNull
+    @Column(name = "name", nullable = false)
     @NotEmpty(message = "Username can't not be empty")
     private String name;
 
     @NonNull
+    @Column(name = "pass", nullable = false)
     @NotEmpty(message = "Password can't not be empty")
     private String pass;
 
+    @Column(name = "email", nullable = false)
     @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
-    @Getter
     @NonNull
     @NotEmpty
     private String email;
+
+    @OneToOne(mappedBy = "user")
+    private UserStorage userStorage;
 }
