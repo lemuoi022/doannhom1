d77678092124c799bfbee6b0275d855cc81ade69
DuckTheMug <duc.12.2002@gmail.com>
2023-11-03 16:43:24 +0700

    add rootPath

--- src/main/java/com/example/danhom1/Storage/UserStorage.java
+++ src/main/java/com/example/danhom1/Storage/UserStorage.java
@@ -12,20 +12,24 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "user_storage")
 public class UserStorage extends Storage {
-
     @Id
     @NonNull
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id", nullable = false, unique = true)
     private Integer id;
 
     @NonNull
-    @NotEmpty
+    @NotEmpty(message = "There must be a default limit.")
     @Column(name = "userLimit", nullable = false)
     private Float userLimit = super.getDefaultLimit();
 
     @NonNull
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "userID",referencedColumnName = "id")
-    private User user = new User();
+    @JoinColumn(name = "userID",referencedColumnName = "id", nullable = false, unique = true)
+    private final User user = new User();
+
+    @NonNull
+    @NotEmpty(message = "There must be a root path.")
+    @Column(name = "rootPath", nullable = false)
+    private final String rootPath = super.getPPath() + user.getName().strip();
 }
