a1adc8b0d68df2d8f0eb1fdad19abc052080ec3d
DuckTheMug <duc.12.2002@gmail.com>
2023-11-03 16:42:39 +0700

    initial UserService

---
+++ src/main/java/com/example/danhom1/User/UserService.java
@@ -0,0 +1,9 @@
+package com.example.danhom1.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private User user;
+}
