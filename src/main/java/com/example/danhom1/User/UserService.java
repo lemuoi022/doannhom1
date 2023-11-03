197758fa254de56de57d3ce4645f0126ec691661
DuckTheMug <duc.12.2002@gmail.com>
2023-11-03 17:36:30 +0700

    initial UserService methods

--- src/main/java/com/example/danhom1/User/UserService.java
+++ src/main/java/com/example/danhom1/User/UserService.java
@@ -1,9 +1,21 @@
 package com.example.danhom1.User;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@AllArgsConstructor
+@Transactional
 public class UserService {
-    private User user;
+    private final UserRepo userRepo;
+    public User RegisterNewUser(User user) throws UserAlreadyExistException {
+        if (EmailExists((user.getEmail())))
+            throw new UserAlreadyExistException();
+        return userRepo.save(user);
+    }
+
+    private Boolean EmailExists(String email){
+        return !userRepo.findAllByEmailIs(email).isEmpty();
+    }
 }
