5b51740e178eddb1743060b7f62d55d9dc7d5ac9
DuckTheMug <duc.12.2002@gmail.com>
2023-11-03 19:59:57 +0700

    cleanup

--- src/main/java/com/example/danhom1/User/UserAlreadyExistException.java
+++ src/main/java/com/example/danhom1/User/UserAlreadyExistException.java
@@ -3,5 +3,4 @@ package com.example.danhom1.User;
 import lombok.experimental.StandardException;
 
 @StandardException
-public class UserAlreadyExistException extends Throwable{
-}
+public class UserAlreadyExistException extends Throwable{ }
