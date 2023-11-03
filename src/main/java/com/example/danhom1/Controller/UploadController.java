6b6a91f1ce8c30143b7230562c5dda0bd9d66668
DuckTheMug <duc.12.2002@gmail.com>
2023-11-03 20:45:52 +0700

    renaming and fixes

--- src/main/java/com/example/danhom1/Controller/UploadController.java
+++ src/main/java/com/example/danhom1/Controller/UploadController.java
@@ -5,17 +5,13 @@ import java.util.stream.Collectors;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -28,8 +24,9 @@ import com.example.danhom1.Storage.StorageService;
 public class UploadController {
     private final StorageService storageService;
 
-    @GetMapping("/files")
-	public String listUploadedFiles(Model model) {
+    @GetMapping("/upload")
+	@ModelAttribute
+	public String listUploadedFiles(@NonNull Model model) {
 		model.addAttribute("files", storageService.loadAll()
         .map(path -> MvcUriComponentsBuilder.fromMethodName(UploadController.class, 
         "serveFile", path.getFileName().toString()).build().toUri().toString()).
@@ -46,11 +43,11 @@ public class UploadController {
 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
 	}
 
-	@PostMapping("/files")
+	@PostMapping("/upload")
 	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
         storageService.store(file);
 		redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
-		return "redirect:/";
+		return "redirect:/upload";
 	}
 
 	@ExceptionHandler(StorageFileNotFoundException.class)
